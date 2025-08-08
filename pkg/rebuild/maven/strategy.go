fbde55ec6418426c1a83d0a5cb0fa42dc8de9e5f
Aman Sharma <amsr@google.com>
2025-08-08 23:23:21 +0000

    Revert "add maven using apk that installs 3.9.5"
    automatically adds java 21 which we don't want
    better to install it via archives

--- pkg/rebuild/maven/strategy.go
+++ pkg/rebuild/maven/strategy.go
@@ -55,12 +55,7 @@ var toolkit = []*flow.Tool{
 			Runs: "apk add openjdk{{.With.version}}",
 		}},
 	},
-	{
-		Name: "maven/setup-maven",
-		Steps: []flow.Step{{
-			Runs: "apk add maven",
-		}},
-	},
+
 	{
 		Name: "maven/deps/basic",
 		Steps: []flow.Step{
@@ -69,9 +64,6 @@ var toolkit = []*flow.Tool{
 				With: map[string]string{
 					"version": "{{.With.version}}",
 				},
-			},
-			{
-				Uses: "maven/setup-maven",
 			},
 		},
 	},
