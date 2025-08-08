87d90db04070d5c35d019dcb87faf6577ba048c6
Aman Sharma <amsr@google.com>
2025-08-08 20:42:19 +0000

    add flow step for downloading java

--- pkg/rebuild/maven/strategy.go
+++ pkg/rebuild/maven/strategy.go
@@ -4,6 +4,9 @@
 package maven
 
 import (
+	"strconv"
+	"strings"
+
 	"github.com/google/oss-rebuild/pkg/rebuild/flow"
 	"github.com/google/oss-rebuild/pkg/rebuild/rebuild"
 )
@@ -24,14 +27,52 @@ func (b *MavenBuild) ToWorkflow() *rebuild.WorkflowStrategy {
 			Uses: "git-checkout",
 		}},
 		Deps: []flow.Step{{
-			Runs: "true",
+			Uses: "maven/deps/basic",
+			With: map[string]string{
+				"version": getOnlyMajorVersion(b.JDKVersion),
+			},
 		}},
 		Build: []flow.Step{{
-			Uses: "true",
+			Runs: "echo 'Building Maven project'",
 		}},
 	}
 }
 
 func (b *MavenBuild) GenerateFor(t rebuild.Target, be rebuild.BuildEnv) (rebuild.Instructions, error) {
 	return b.ToWorkflow().GenerateFor(t, be)
+}
+
+func init() {
+	for _, t := range toolkit {
+		flow.Tools.MustRegister(t)
+	}
+}
+
+var toolkit = []*flow.Tool{
+	{
+		Name: "maven/setup-java",
+		Steps: []flow.Step{{
+			Runs: "apk add openjdk{{.With.version}}",
+		}},
+	},
+
+	{
+		Name: "maven/deps/basic",
+		Steps: []flow.Step{
+			{
+				Uses: "maven/setup-java",
+				With: map[string]string{
+					"version": "{{.With.version}}",
+				},
+			},
+		},
+	},
+}
+
+func getOnlyMajorVersion(version string) string {
+	if major, err := strconv.Atoi(version); err == nil {
+		return strconv.Itoa(major)
+	}
+	parts := strings.Split(version, ".")
+	return parts[0]
 }
