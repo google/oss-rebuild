c197cc871f24d4603ef48ca2a683081377191f64
Aman Sharma <amsr@google.com>
2025-08-12 09:38:32 -0400

    fix URL of JDK 8

--- pkg/rebuild/maven/jdk_urls.go
+++ pkg/rebuild/maven/jdk_urls.go
@@ -51,7 +51,7 @@ var JDKDownloadURLs = map[string]string{
 	"9.0.4":    "https://download.java.net/java/GA/jdk9/9.0.4/binaries/openjdk-9.0.4_linux-x64_bin.tar.gz",
 	"9.0.1":    "https://download.java.net/java/GA/jdk9/9.0.1/binaries/openjdk-9.0.1_linux-x64_bin.tar.gz",
 	"9":        "https://download.java.net/java/GA/jdk9/9/binaries/openjdk-9_linux-x64_bin.tar.gz",
-	"8":        "https://github.com/adoptium/temurin8-binaries/releases/download/jdk8u462-b08/OpenJDK8U-debugimage_x64_linux_hotspot_8u462b08.tar.gz",
+	"8":        "https://github.com/adoptium/temurin8-binaries/releases/download/jdk8u462-b08/OpenJDK8U-jdk_x64_linux_hotspot_8u462b08.tar.gz",
 	"8u441":    "https://cfdownload.adobe.com/pub/adobe/coldfusion/java/java8/java8u441/jdk/jdk-8u441-linux-x64.tar.gz",
 	"8u431":    "https://cfdownload.adobe.com/pub/adobe/coldfusion/java/java8/java8u431/jdk/jdk-8u431-linux-x64.tar.gz",
 	"8u421":    "https://cfdownload.adobe.com/pub/adobe/coldfusion/java/java8/java8u421/jdk/jdk-8u421-linux-x64.tar.gz",
--- pkg/rebuild/maven/strategy_test.go
+++ pkg/rebuild/maven/strategy_test.go
@@ -35,7 +35,7 @@ func TestMavenStrategies(t *testing.T) {
 				SystemDeps: []string{"git", "wget"},
 				Source:     "git clone https://github.com/ldapchai/ldapchai.git .\ngit checkout --force 'a9de4ccc8db9a4862f3819f3dfb63e57a6450bdf'",
 				Deps: `mkdir -p /opt/jdk
-wget -q -O - "https://adoptium.net/download?link=https%3A%2F%2Fgithub.com%2Fadoptium%2Ftemurin8-binaries%2Freleases%2Fdownload%2Fjdk8u462-b08%2FOpenJDK8U-jdk_x64_linux_hotspot_8u462b08.tar.gz&vendor=Adoptium" | tar -xzf - --strip-components=1 -C /opt/jdk
+wget -q -O - "https://github.com/adoptium/temurin8-binaries/releases/download/jdk8u462-b08/OpenJDK8U-jdk_x64_linux_hotspot_8u462b08.tar.gz" | tar -xzf - --strip-components=1 -C /opt/jdk
 export JAVA_HOME=/opt/jdk
 export PATH=$JAVA_HOME/bin:$PATH`,
 				Build:      "echo 'Building Maven project'", // TODO: Replace with actual Maven build command
