49a6e7b46f6cd70b430f2b8a8b1ad52fbe05fa65
Aman Sharma <amsr@google.com>
2025-08-14 11:58:43 -0400

    feat: setup JDK for MavenBuild strategy (#685)

--- pkg/rebuild/maven/strategy.go
+++ pkg/rebuild/maven/strategy.go
@@ -4,6 +4,9 @@
 package maven
 
 import (
+	"github.com/google/oss-rebuild/internal/textwrap"
+	"github.com/pkg/errors"
+
 	"github.com/google/oss-rebuild/pkg/rebuild/flow"
 	"github.com/google/oss-rebuild/pkg/rebuild/rebuild"
 )
@@ -17,21 +20,64 @@ type MavenBuild struct {
 
 var _ rebuild.Strategy = &MavenBuild{}
 
-func (b *MavenBuild) ToWorkflow() *rebuild.WorkflowStrategy {
+func (b *MavenBuild) ToWorkflow() (*rebuild.WorkflowStrategy, error) {
+	jdkVersionURL, exists := JDKDownloadURLs[b.JDKVersion]
+	if !exists {
+		return nil, errors.Errorf("no download URL for JDK version %s", b.JDKVersion)
+	}
 	return &rebuild.WorkflowStrategy{
 		Location: b.Location,
 		Source: []flow.Step{{
 			Uses: "git-checkout",
 		}},
 		Deps: []flow.Step{{
-			Runs: "true",
+			Uses: "maven/deps/basic",
+			With: map[string]string{
+				"versionURL": jdkVersionURL,
+			},
 		}},
 		Build: []flow.Step{{
-			Uses: "true",
+			Runs: "echo 'Building Maven project'",
 		}},
-	}
+	}, nil
 }
 
 func (b *MavenBuild) GenerateFor(t rebuild.Target, be rebuild.BuildEnv) (rebuild.Instructions, error) {
-	return b.ToWorkflow().GenerateFor(t, be)
+	workflow, err := b.ToWorkflow()
+	if err != nil {
+		return rebuild.Instructions{}, err
+	}
+	return workflow.GenerateFor(t, be)
+}
+
+func init() {
+	for _, t := range toolkit {
+		flow.Tools.MustRegister(t)
+	}
+}
+
+var toolkit = []*flow.Tool{
+	{
+		Name: "maven/setup-java",
+		Steps: []flow.Step{{
+			// TODO: ensure that the environment variables JAVA_HOME and PATH are also available for the build step of strategy
+			Runs: textwrap.Dedent(`
+				mkdir -p /opt/jdk
+				wget -q -O - "{{.With.versionURL}}" | tar -xzf - --strip-components=1 -C /opt/jdk
+				export JAVA_HOME=/opt/jdk
+				export PATH=$JAVA_HOME/bin:$PATH`[1:]),
+			Needs: []string{"wget"},
+		}},
+	},
+	{
+		Name: "maven/deps/basic",
+		Steps: []flow.Step{
+			{
+				Uses: "maven/setup-java",
+				With: map[string]string{
+					"versionURL": "{{.With.versionURL}}",
+				},
+			},
+		},
+	},
 }
---
+++ pkg/rebuild/maven/strategy_test.go
@@ -0,0 +1,75 @@
+// Copyright 2025 Google LLC
+// SPDX-License-Identifier: Apache-2.0
+
+package maven
+
+import (
+	"testing"
+
+	"github.com/google/go-cmp/cmp"
+	"github.com/google/oss-rebuild/pkg/rebuild/rebuild"
+)
+
+func TestMavenStrategies(t *testing.T) {
+	tests := []struct {
+		name     string
+		strategy rebuild.Strategy
+		want     rebuild.Instructions
+		wantErr  bool
+	}{
+		{
+			"return build instructions for a valid JDK version",
+			&MavenBuild{
+				Location: rebuild.Location{
+					Repo: "https://foo.bar",
+					Ref:  "ref",
+					Dir:  "dir",
+				},
+				JDKVersion: "11.0.1",
+			},
+			rebuild.Instructions{
+				Location: rebuild.Location{
+					Repo: "https://foo.bar",
+					Ref:  "ref",
+					Dir:  "dir",
+				},
+				SystemDeps: []string{"git", "wget"},
+				Source:     "git clone https://foo.bar .\ngit checkout --force 'ref'",
+				Deps: `mkdir -p /opt/jdk
+wget -q -O - "https://download.java.net/java/GA/jdk11/13/GPL/openjdk-11.0.1_linux-x64_bin.tar.gz" | tar -xzf - --strip-components=1 -C /opt/jdk
+export JAVA_HOME=/opt/jdk
+export PATH=$JAVA_HOME/bin:$PATH`,
+				Build:      "echo 'Building Maven project'", // TODO: Replace with actual Maven build command
+				OutputPath: "ldapchai-0.8.6.jar",
+			},
+			false,
+		},
+		{
+			"throw an error if JDK installation candidate is not found",
+			&MavenBuild{
+				Location: rebuild.Location{
+					Repo: "https://foo.bar",
+					Ref:  "ref",
+					Dir:  "repo-dir",
+				},
+				JDKVersion: "30",
+			},
+			rebuild.Instructions{},
+			true,
+		},
+	}
+	for _, tc := range tests {
+		t.Run(tc.name, func(t *testing.T) {
+			inst, err := tc.strategy.GenerateFor(rebuild.Target{Ecosystem: rebuild.Maven, Package: "com.github.ldapchai:ldapchai", Version: "0.8.6", Artifact: "ldapchai-0.8.6.jar"}, rebuild.BuildEnv{})
+			if err != nil && !tc.wantErr {
+				t.Fatalf("%s: Strategy%v.GenerateFor() failed unexpectedly: %v", tc.name, tc.strategy, err)
+			}
+			if tc.wantErr && (err == nil) {
+				t.Fatalf("%s: Strategy%v.GenerateFor() should fail but did not", tc.name, tc.strategy)
+			}
+			if diff := cmp.Diff(inst, tc.want); diff != "" {
+				t.Errorf("Strategy%v.GenerateFor() returned diff (-got +want):\n%s", tc.strategy, diff)
+			}
+		})
+	}
+}
