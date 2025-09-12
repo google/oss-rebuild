deb45efb9d03766d92b15385a32dbe50d9d80343
Aman Sharma <amsr@google.com>
2025-09-12 11:35:16 -0400

    Add support for Gradle inference (#766)

--- pkg/rebuild/maven/strategy_test.go
+++ pkg/rebuild/maven/strategy_test.go
@@ -11,7 +11,7 @@ import (
 	"github.com/google/oss-rebuild/pkg/rebuild/rebuild"
 )
 
-func TestMavenStrategies(t *testing.T) {
+func TestStrategies(t *testing.T) {
 	tests := []struct {
 		name     string
 		strategy rebuild.Strategy
@@ -44,6 +44,35 @@ func TestMavenStrategies(t *testing.T) {
 					export PATH=$JAVA_HOME/bin:$PATH
 					mvn clean package -DskipTests`[1:]),
 				OutputPath: "dir/target/ldapchai-0.8.6.jar",
+			},
+			false,
+		},
+		{
+			"return gradle build instructions for a valid JDK version",
+			&GradleBuild{
+				Location: rebuild.Location{
+					Repo: "https://foo.bar",
+					Ref:  "ref",
+					Dir:  "dir",
+				},
+				JDKVersion: "11.0.1",
+			},
+			rebuild.Instructions{
+				Location: rebuild.Location{
+					Repo: "https://foo.bar",
+					Ref:  "ref",
+					Dir:  "dir",
+				},
+				SystemDeps: []string{"git", "wget"},
+				Source:     "git clone https://foo.bar .\ngit checkout --force 'ref'",
+				Deps: textwrap.Dedent(`
+					mkdir -p /opt/jdk
+					wget -q -O - "https://download.java.net/java/GA/jdk11/13/GPL/openjdk-11.0.1_linux-x64_bin.tar.gz" | tar -xzf - --strip-components=1 -C /opt/jdk`)[1:],
+				Build: textwrap.Dedent(`
+					export JAVA_HOME=/opt/jdk
+					export PATH=$JAVA_HOME/bin:$PATH
+					./gradlew assemble --no-daemon -Pversion=0.8.6`[1:]),
+				OutputPath: "dir/build/libs/ldapchai-0.8.6.jar",
 			},
 			false,
 		},
