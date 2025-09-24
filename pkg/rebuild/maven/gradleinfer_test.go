b109b14f1afa36caeb3948e43d58a561f2cac7c9
Aman Sharma <amsr@google.com>
2025-09-24 16:22:06 -0400

    Test for gradle inference

--- pkg/rebuild/maven/gradleinfer_test.go
+++ pkg/rebuild/maven/gradleinfer_test.go
@@ -4,9 +4,13 @@
 package maven
 
 import (
+	"archive/zip"
 	"testing"
 
 	"github.com/google/oss-rebuild/internal/gitx/gitxtest"
+	"github.com/google/oss-rebuild/pkg/archive"
+	"github.com/google/oss-rebuild/pkg/rebuild/rebuild"
+	"github.com/google/oss-rebuild/pkg/registry/maven"
 )
 
 func TestFindBuildGradleDir(t *testing.T) {
@@ -106,6 +110,162 @@ func TestFindBuildGradleDir(t *testing.T) {
 					t.Errorf("findBuildGradleDir() = %q, want %q", actualDir, tc.expectedDir)
 				}
 			}
+		})
+	}
+}
+
+func TestGradleInfer(t *testing.T) {
+	testCases := []struct {
+		name           string
+		target         rebuild.Target
+		repo           string
+		zipEntries     map[string][]*archive.ZipEntry
+		expectedCommit string
+		wantErr        bool
+	}{
+		{
+			name: "infer using tag heuristic",
+			target: rebuild.Target{
+				Package:   "com.example:myapp",
+				Version:   "1.0.0",
+				Ecosystem: rebuild.Maven,
+			},
+			repo: `
+            commits:
+            - id: initial-commit
+              files:
+                gradle.properties: |
+                  group=com.example
+                build.gradle: |
+                  repositories {
+                    mavenCentral()
+                  }
+            - id: second-commit
+              parents: [initial-commit]
+              tags: ['v1.0.0']
+              files:
+                README.md: |
+                    Sample Project`,
+			zipEntries: map[string][]*archive.ZipEntry{
+				maven.TypeJar: {
+					{
+						FileHeader: &zip.FileHeader{Name: "META-INF/maven/com/example/myapp/pom.xml"},
+						Body:       []byte("<project><groupId>com.example</groupId><artifactId>myapp</artifactId><version>1.0.0</version></project>"),
+					},
+					{
+						FileHeader: &zip.FileHeader{Name: "META-INF/MANIFEST.MF"},
+						Body:       []byte("Manifest-Version: 1.0\nBuild-Jdk: 11.0.1\n"),
+					},
+				},
+			},
+			// select second-commit because it has the tag matching version 1.0.0
+			expectedCommit: "second-commit",
+		},
+		{
+			name: "infer using source jar heuristic",
+			target: rebuild.Target{
+				Package:   "com.example:myapp",
+				Version:   "1.0.0",
+				Ecosystem: rebuild.Maven,
+			},
+			repo: `
+            commits:
+            - id: initial-commit
+              files:
+                build.gradle: |
+                  group=com.example
+                src/main/java/App.java: |
+                  class App {}
+            - id: second-commit
+              parents: [initial-commit]
+              files:
+                build.gradle: |
+                  group=com.example
+                src/main/java/App.java: |
+                  public class App {}`,
+			zipEntries: map[string][]*archive.ZipEntry{
+				maven.TypeJar: {
+					{
+						FileHeader: &zip.FileHeader{Name: "META-INF/maven/com/example/myapp/pom.xml"},
+						Body:       []byte("<project><groupId>com.example</groupId><artifactId>myapp</artifactId><version>1.0.0</version></project>"),
+					},
+					{
+						FileHeader: &zip.FileHeader{Name: "META-INF/MANIFEST.MF"},
+						Body:       []byte("Manifest-Version: 1.0\nBuild-Jdk: 11.0.1\n"),
+					},
+				},
+				maven.TypeSources: {
+					{
+						FileHeader: &zip.FileHeader{Name: "src/main/java/App.java"},
+						Body:       []byte("public class App {}"),
+					},
+				},
+			},
+			// select second-commit because it is the closest to the source jar creation time
+			expectedCommit: "second-commit",
+		},
+		{
+			name: "fail when no heuristics match",
+			target: rebuild.Target{
+				Package:   "com.example:unknown",
+				Version:   "0.1.0",
+				Ecosystem: rebuild.Maven,
+			},
+			repo: `
+            commits:
+            - id: initial-commit
+              tags: ['4.2.0']
+              files:
+                build.gradle: |
+                  group=com.example
+                src/main/java/Main.java: |
+                  public class Main {}`,
+			zipEntries: map[string][]*archive.ZipEntry{
+				maven.TypeJar: {
+					{
+						FileHeader: &zip.FileHeader{Name: "META-INF/maven/com/example/unknown/pom.xml"},
+						Body:       []byte("<project><groupId>com.example</groupId><artifactId>unknown</artifactId><version>0.1.0</version></project>"),
+					},
+					{
+						FileHeader: &zip.FileHeader{Name: "META-INF/MANIFEST.MF"},
+						Body:       []byte("Manifest-Version: 1.0\nBuild-Jdk: 11.0.1\n"),
+					},
+				},
+			},
+			wantErr: true,
+		},
+	}
+	for _, tc := range testCases {
+		t.Run(tc.name, func(t *testing.T) {
+			repoConfig := &rebuild.RepoConfig{}
+			repo, err := gitxtest.CreateRepoFromYAML(tc.repo, nil)
+			if err != nil {
+				t.Fatalf("CreateRepoFromYAML() error = %v", err)
+			}
+			repoConfig.Repository = repo.Repository
+			mockRegistry := &mockMavenRegistry{
+				artifactCoordinates: make(map[artifactCoordinates][]byte),
+			}
+			addArtifacts(mockRegistry, tc.zipEntries, tc.target)
+			mockMux := rebuild.RegistryMux{
+				Maven: mockRegistry,
+			}
+			got, err := GradleInfer(t.Context(), tc.target, mockMux, repoConfig)
+			if tc.wantErr {
+				if err == nil {
+					t.Fatalf("GradleInfer() = %v, want error", got)
+				}
+			} else {
+				if err != nil {
+					t.Fatalf("GradleInfer() error = %v", err)
+				}
+				actualCommit := got.(*GradleBuild).Location.Ref
+				expectedCommit := repo.Commits[tc.expectedCommit].String()
+				if actualCommit != expectedCommit {
+					t.Errorf("GradleInfer() commit = %q, want %q", actualCommit, tc.expectedCommit)
+				}
+			}
+
 		})
 	}
 }
