249ed9b3e1b1ad5c9e1f07736027a4f3a376d00c
Matthew Suozzo <msuozzo@google.com>
2026-01-27 16:53:12 +0000

    Reduce fixed sleep time in tests
    These show up as extremely long-running tests when run with -count N, at
    least as a proportion of the amount of work they're doing. These changes
    shouldn't impact the strength of these tests but will reduce the overall
    length of execution.
    
    This puts our full suite at under 2min for -count 100.

--- internal/gcb/gcb_test.go
+++ internal/gcb/gcb_test.go
@@ -70,7 +70,7 @@ func TestDoBuildTimeoutNoTerminate(t *testing.T) {
 			return nil
 		},
 	}
-	ctx, cancel := context.WithTimeout(context.Background(), 1*time.Second)
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Millisecond)
 	defer cancel()
 	b, err := DoBuild(ctx, c, "project", &cloudbuild.Build{}, DoBuildOpts{TerminateOnTimeout: false})
 	if !errors.Is(err, context.DeadlineExceeded) {
