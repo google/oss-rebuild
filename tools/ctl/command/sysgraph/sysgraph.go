7068e58d8175b11cd51b2901451ec8e16cf59ee5
Matthew Suozzo <msuozzo@google.com>
2026-03-08 22:04:31 +0000

    Add cli support for sysgraph netlog annotation
    forge-parent: myknlruqvvpo

---
+++ tools/ctl/command/sysgraph/sysgraph.go
@@ -0,0 +1,19 @@
+// Copyright 2026 Google LLC
+// SPDX-License-Identifier: Apache-2.0
+
+package sysgraph
+
+import (
+	"github.com/google/oss-rebuild/tools/ctl/command/sysgraph/annotatenetwork"
+	"github.com/spf13/cobra"
+)
+
+// Command returns the sysgraph parent command.
+func Command() *cobra.Command {
+	cmd := &cobra.Command{
+		Use:   "sysgraph",
+		Short: "Sysgraph analysis and transformation commands",
+	}
+	cmd.AddCommand(annotatenetwork.Command())
+	return cmd
+}
