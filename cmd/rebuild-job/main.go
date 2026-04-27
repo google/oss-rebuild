ba397f42f00c6f3adf4995f9a4034db150ed5227
William Burton <williamburton@google.com>
2026-04-27 14:57:51 -0400

    Set default gcb executor timeout to 23 hours (#1226)
    Set default GCB executor deadline to 23 hours

--- cmd/rebuild-job/main.go
+++ cmd/rebuild-job/main.go
@@ -56,8 +56,11 @@ func main() {
 	if err != nil {
 		log.Fatalf("parsing service location: %v", err)
 	}
-	ctx, cancel := context.WithTimeout(ctx, req.BuildTimeout)
-	defer cancel()
+	if req.BuildTimeout != 0 {
+		var cancel context.CancelFunc
+		ctx, cancel = context.WithTimeout(ctx, req.BuildTimeout)
+		defer cancel()
+	}
 	if _, err := apiservice.RebuildPackage(ctx, req, deps); err != nil {
 		log.Fatalf("Rebuilding package: %v", err)
 	}
